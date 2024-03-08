13ee065d7b101693d10cb7c4629102b669eb4d53
Muhammad Ryan <132818688+mazyans@users.noreply.github.com>
2024-03-08 22:39:18 +0700

    Add files via upload

---
+++ Pertemuan 1/test1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+print "<pre>";
+print "<center>";
+print "<tr><td><font size = 16 color = 'red'><strong>BIODATA </strong></font></td></tr>";
+print "<br><br>";
+print "<table align='center'>";
+print "<tr><td><font size = 12 color='blue'>Nama        : <strong>Muhammad Ryan Aziza Parasudewa</strong></font></td></tr>";
+print "<tr><td><font size = 12 color='blue'>Email       : mryanap1512@gmail.com</font></td></tr>";
+print "<tr><td><font size = 12 color='blue'>Jurusan     : Teknik Informatika</font></td></tr>";
+print "<tr><td><font size = 12 color='blue'>Hobby       : Riding Motorcycle</font></td></tr>";
+print "</table>";
+print "</center>";
+?>
+</body>
+</html>
